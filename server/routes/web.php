43f80a209408da8a5ffdebb49a3014c8a4916b62
wataru nakanishi <wataru@watarunoMacBook-Pro.local>
2021-01-17 19:13:32 +0900

    add routing

--- server/routes/web.php
+++ server/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{any}', function () {
+    return file_get_contents(base_path('public/out/index.html'));
+})->where('any', '.*');
+
+Route::fallback(function () {
+    return file_get_contents(public_path('public/out/index.html'));
 });
